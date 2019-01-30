f71d6aaf7b5c0767ae14fafaf125f0fa87053135
Eric Stone <stone11375@gmail.com>
2019-01-30 20:22:52 +0000

    Added comments

--- matrix_script.py
+++ matrix_script.py
@@ -1,4 +1,9 @@
 def matrix_script(n, m, matrix):
+    
+    """
+    These are regex expressions I use later
+    """
+    
     regex_char = r"[A-Z, a-z, 0-9]"
     regex_char_end = r"([A-Z, a-z, 0-9]+)*$"
     regex_nonchar = r"([^A-Z, a-z, 0-9]+|\s+)*"
@@ -9,6 +14,10 @@ def matrix_script(n, m, matrix):
     i = 0
     matrix_unwrap = ""
     
+    """
+    This unravels the matrix input and returns it in a simple string
+    """
+    
     while i < m:
         for each in matrix:
             matrix_unwrap += each[i]
@@ -16,13 +25,32 @@ def matrix_script(n, m, matrix):
     i = 0
     
     while i < len(matrix_unwrap):
+        
+        """
+        This is an initial test for cases where the string starts with a non-letter/number
+        """
+        
         while not matrix_unwrap[i].isalnum() and not re.match(regex_nonchar_end, matrix_unwrap[i:]):
             i += len(re.match(regex_nonchar, matrix_unwrap[i:]).group())
+            
+        """
+        This passes consecutive letters/numbers
+        """
         while matrix_unwrap[i].isalnum():
             i += len(re.match(regex_char, matrix_unwrap[i:]).group())
+            
+        """
+        This replaces groups of non-letter/number between letter/numbers with a space character
+        """
+        
         while not re.match(regex_nonchar_end, matrix_unwrap[i:]):
             matrix_unwrap = matrix_unwrap.replace(re.match(regex_nonchar, matrix_unwrap[i:]).group(), " ", 1)
             break
+        
+        """
+        This checks if the cursor has approached the final group of characters and if so, ends the function
+        """
+        
         while re.match(regex_nonchar_end, matrix_unwrap[i:]) or re.match(regex_char_end, matrix_unwrap[i:]):
             return (matrix_unwrap)
         i += 1
